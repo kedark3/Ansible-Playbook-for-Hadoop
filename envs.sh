2fcf2b5c377e5d2d8218675c1eeff0f8178d9482
Kedar Kulkarni <kedar.kulkarni0@gmail.com>
2016-10-18 01:09:16 -0400

    Making some minor changes to installation

--- envs.sh
+++ envs.sh
@@ -2,6 +2,7 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_PREFIX=/usr/local/hadoop
 # Some convenient aliases and functions for running Hadoop-related commands
 unalias fs &> /dev/null
 alias fs="hadoop fs"
